a5d86280bdf1871cb8305999739e65c46498368a
wabc1994 <liuxiongcheng145@gmail.com>
2018-06-14 10:41:47 +0800

    如何爬梯子的解决方法

---
+++ src/climb_staris.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class climb_staris {
+    /*
+
+采用数组（或者HashMap类的）保存中间一个结果，每一次先判断数组中是否已经出现了包含了
+结果，如果包括结果了，直接返回结果表示，如果没有包括，继续向下做下去
+空间复杂度为O(N), 时间复杂度也为O(n);
+ */
+    public int climbstairs(int n)
+    {
+        int [] memo = new int[n+1];
+        return climb(0,n,memo);
+
+    }
+    public int climb(int i, int n, int memo[])
+    {
+        if(i>n)
+            return 0;
+        if(i==n)
+            return 1;
+        if(memo[i]>0)
+        {
+            return memo[i];
+        }
+        memo[i] = climb(i+1,n,memo) + climb(i+2,n,memo);
+        return memo[i]; }
+    /*
+    * 动态规划算法基础,时间复杂度和空间复杂度同上面一样，数组记录中间结果，但我们最终返回的不需要中间结果，
+    * 不需要一个这么长的数组
+    *
+    * */
+    public int soultion2(int n)
+    {
+        int [] dp = new int [n+1];
+        if(n<2)
+            return 1;
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i=3; i<=n; i++)
+        {
+            dp[i] = dp[i-2] + dp[i-1];
+
+        }
+        return dp[n];
+
+    }
+
+}
+
+
