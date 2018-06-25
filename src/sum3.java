502ae642aed07041a4ed773f19374569a5765378
wabc1994 <liuxiongcheng145@gmail.com>
2018-06-25 15:44:29 +0800

    添加一个hashset方法实现对相同元素的检测，不用分别单独处理三个位置的相同元素

--- src/sum3.java
+++ src/sum3.java
@@ -2,7 +2,8 @@ import java.util.*;
 
 public class sum3 {
 
-
+// 给一个数组，找出其中三个数，等于 0，找出所有的可能。
+// 找到满足题意的加入结果中
     public List<List<Integer>> threeSum(int[] nums) {
         if (nums.length < 3) return null;
         Arrays.sort(nums);
@@ -10,27 +11,32 @@ public class sum3 {
         final int target = 0;
         //处理三个位置的相同元素， i处背后的元素， j背后的元素， k和k之前的元素，相比较
 
-        for (int i = 0; i < nums.length - 2; ++i) {
+        for(int i = 0; i < nums.length - 2; ++i) {
             //相同的数字处理一次即可， i j , k
             // 其中 i固定，j 初始化为i+1, k= nums.length-1(最后一个元素)，
             // 分解为两两相加的情况，
 
-
             if (i > 0 && nums[i] == nums[i - 1]) continue;
-
 
             int j = i + 1;
             int k = nums.length - 1;
+            //类似快速排序算法，左右
             while (j < k) {
+                //往右走，
                 if (nums[i] + nums[j] + nums[k] < target) {
                     ++j;
                     while (nums[j] == nums[j - 1] && j < k) ++j;
-                } else if (nums[i] + nums[j] + nums[k] > target) {
+                    }
+                    //往左走，
+                else if (nums[i] + nums[j] + nums[k] > target) {
                     --k;
                     while (nums[k] == nums[k + 1] && j < k) --k;
-                } else {
+                }
+                //找到合适的位置的情况，类似于快速排序找到合适的位置插入
+                else {
                     res.add(Arrays.asList(nums[i], nums[j], nums[k]));
                     //
+                    //两个同时走
                     ++j;
                     --k;
                     //处理相邻的相同元素，只处理一次，
@@ -74,9 +80,38 @@ public class sum3 {
             }
         }
         return res;
-
     }
 
+    public List<List<Integer>> solution_3(int []nums, int target) {
+        //采用简单的hashset来处理相同的元素，不用分别在三个位置单独处理相同元素,
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums.length < 3)
+            return null;
+        HashSet<List<Integer>> hs = new HashSet<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 3; i++) {
+            int j = i + 1;
+            int m = nums.length - 1;
+            while (i < m) {
+                int sum = nums[i] + nums[j] + nums[m];
+                if (sum < target)
+                    ++j;
+                else if (sum > target)
+                    --m;
+                else {
+                    //先处理再添加
+                    ArrayList<Integer> tmp = new ArrayList<>(Arrays.asList(nums[i], nums[j], nums[m]));
+                    if (!hs.contains(tmp)) {
+                        hs.add(tmp);
+                        res.add(tmp);
+                    }
+                    --m;
+                    ++j;
+                }
+            }
+        }
+         return res;
+    }
 
 
 }
