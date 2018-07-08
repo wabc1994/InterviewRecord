4e639748092aac5ab48bbb9e853689bed79293a4
wabc1994 <liuxiongcheng145@gmail.com>
2018-07-08 21:48:51 +0800

    基础：逆置字符串，reverse, 进位/2， 字符串转换成为数字，结果位 %2，考虑最大字符串长度

---
+++ src/AddBinary.java
@@ -0,0 +1,20 @@
+public class AddBinary {
+    public String solution_1(String a, String b){
+        StringBuilder sb = new StringBuilder();
+        int i = a.length() - 1, j = b.length() -1, carry = 0;
+        while (i >= 0 || j >= 0) {
+            int sum = carry;
+            if (j >= 0) sum += b.charAt(j--) - '0';
+            if (i >= 0) sum += a.charAt(i--) - '0';
+            sb.append(sum % 2);
+            carry = sum / 2;
+        }
+        if (carry != 0) sb.append(carry);
+        return sb.reverse().toString();
+
+    }
+    public static void main(String[] args) {
+        AddBinary test= new AddBinary();
+        System.out.println(test.solution_1("11","1"));
+    }
+}
