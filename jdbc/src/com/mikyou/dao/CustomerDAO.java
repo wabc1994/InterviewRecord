4aecffee4209d39c55ac018743d0557316aace27
wabc1994 <liuxiongcheng145@gmail.com>
2018-09-03 09:49:55 +0800

    修改了一些文件

--- jdbc/src/com/mikyou/dao/CustomerDAO.java
+++ jdbc/src/com/mikyou/dao/CustomerDAO.java
@@ -11,81 +11,62 @@ import java.util.List;
 
 //dao 层调用 JDBCTemplate的一个对象情况，实现真正的业务
 public class CustomerDAO implements OnIMapperListener<Customer> {
-    private JDBCTemplate<Customer> temp;
-
-
+    private JDBCTemplate<Customer>temp;
     public CustomerDAO() {
         temp=new JDBCTemplate<Customer>();
         temp.setOnMapperListener(this);//给CustomerDAO注册映射监听器
     }
-
-
     /**
      * 以下三个方法都有一个共同的特点就是无需带有返回结果集，所以直接在JDBCTemplate模板类中直接封装成一个update方法即可
      * */
-
-
     public void save(Customer customer) throws SQLException{
         String sql="insert into tb1_customer(name,gender,telephone,address) values(?,?,?,?)";
         Object[] agrs={customer.getName(),customer.getGender(),customer.getTelephone(),customer.getAddress()};
-        temp.update(sql,agrs);
+        temp.update(sql, agrs);
     }
-
-
-
     public void update(Customer customer){
         String sql="update tb1_customer set name=?,gender=?,telephone=?,address=? where id=?";
-        //字段值的设计
         Object[] agrs={customer.getName(),customer.getGender(),customer.getTelephone(),customer.getAddress(),customer.getId()};
+        temp.update(sql, agrs);
+    }
+    public void delete(long id){
+        String sql="delete from tb1_customer where id=?";
+        Object[] agrs={id};
         temp.update(sql, agrs);
     }
 
 
-    public void delete(long id){
-        String sql = "delete from tb1_customer where id=?";
-        Object [] agrs = {id};
-        temp.update(sql,agrs);
-    }
-
     public Customer findByName(String name){
-        String sql = "select * from tb1_customer where name=?";
-      //代表从那一列来查询数据
-        Object[] args ={name};
-        Customer customer = temp.queryOne(sql,args);
+        String sql="select * from tb1_customer where name=?";
+        Object[] args={name};
+        Customer customer=temp.queryOne(sql, args);
         return customer;
-
     }
-
-
     public List<Customer> findAll(){
-        String sql ="select * from tb1_customer";
-        Object[] args = {};
-        return temp.queryAll(sql,args);
-
+        String sql="select * from tb1_customer";
+        Object[] args={};
+        return temp.queryAll(sql, args);
     }
-
-/**
- * 具体实现相应的回调方法
- * @param rs 回调返回的结果集
- * @deprecated:处理结果集 并且返回的类型是我们的Bean类型Customer
-
- */
-
+    /**
+     * @Mikyou
+     * 具体实现相应的回调方法
+     * @param rs 回调返回的结果集
+     * @deprecated:处理结果集 并且返回的类型是我们的Bean类型Customer
+     * */
     @Override
     public Customer mapper(ResultSet rs) {
-        Customer customer = null;
-        //关系映射，监听器，rs代表sql结果集等基础知识
-        try{
-            Long id = rs.getLong("id");
-            String name = rs.getString("name");
-            String gender = rs.getString("gender");
-            String address =  rs.getString("addresss");
-            String telephone = rs.getString("telephone");
-            //从数据库中获得原始的数据然后构造新的对象
-            customer = new Customer(id, name,gender,telephone,address);
-        }catch (SQLException e){
+        Customer customer=null;
+        try {
+            Long id=rs.getLong("id");
+            String name=rs.getString("name");
+            String gender=rs.getString("gender");
+            String address=rs.getString("address");
+            String telephone=rs.getString("telephone");
+            customer=new Customer(id, name, gender, telephone, address);
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return customer;
     }
+
 }
--- jdbc/src/com/mikyou/test/DaoTest.java
+++ jdbc/src/com/mikyou/test/DaoTest.java
@@ -1,39 +1,54 @@
 package com.mikyou.test;
 
 import com.mikyou.bean.Customer;
+import com.mikyou.common.JDBCTemplate;
+import com.mikyou.common.OnIMapperListener;
 import com.mikyou.dao.CustomerDAO;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
-public class DaoTest {
-    /**
-     测试类，在这里实现的业务逻辑类似与service 等情况，service层的接口代码跟dao层是一样的情况
-     */
-    private  CustomerDAO customerDAO;
-
-    public DaoTest() {
-        this.customerDAO =new CustomerDAO();
+public class DaoTest  {
+    public static void main(String[] args) {
+        new DaoTest().add();//增
+       // new DaoTest().delete();//删
+        // new DaoTest().update();//改
+        //new DaoTest().findOne();//查询具体某一个
+        // new DaoTest().findAll();//查询一个集合
     }
 
-    private void findAll(){
-        //service层调用 customerDAO对象即可，完成对数据库的操作过程等情况
-        List<Customer> customers = customerDAO.findAll();
-        for(Customer customer:customers){
-            System.out.println(customer.toString());
-        }
-    }
-
-    private void findOne()
-    {
+    private void findAll() {
+        // TODO Auto-generated method stub
+        CustomerDAO customerDAO=new CustomerDAO();
         List<Customer> customers=customerDAO.findAll();
         for (Customer customer : customers) {
             System.out.println(customer.toString());
         }
 
-
     }
+
+    private void findOne() {
+        // TODO Auto-generated method stub
+        CustomerDAO customerDAO=new CustomerDAO();
+        Customer customer=customerDAO.findByName("Bob");
+        System.out.println(customer.toString());
+    }
+
+    private void update() {
+        // TODO Auto-generated method stub
+        CustomerDAO customerDAO=new CustomerDAO();
+        customerDAO.update(new Customer(2L, "Bob", "男", "123456789", "英国"));
+    }
+
+    private void delete() {
+        // TODO Auto-generated method stub
+        CustomerDAO customerDAO=new CustomerDAO();
+        customerDAO.delete(1L);
+    }
+
     private void add() {
+        CustomerDAO customerDAO=new CustomerDAO();
         try {
             Customer customer=new Customer(1L, "Mikyou", "男", "123456789", "中国");
             customerDAO.save(customer);
@@ -45,5 +60,7 @@ public class DaoTest {
         }
 
     }
+
+
 
 }
