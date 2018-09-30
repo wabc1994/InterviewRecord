9b91c822cd7b4897668cbd4b201c629e8f08b74e
wabc1994 <liuxiongcheng145@gmail.com>
2018-09-30 16:57:51 +0800

    如何从中序遍历和前序遍历中构建一个树

---
+++ src/Tree/MaximumDepthofBinaryTree.java
@@ -0,0 +1,21 @@
+package Tree;
+
+/**
+ * Given binary tree [3,9,20,null,null,15,7],
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ *   return its depth = 3.
+ */
+public class
+
+
+
+MaximumDepthofBinaryTree {
+    public int maxDepth(TreeNode root) {
+
+    }
+}
