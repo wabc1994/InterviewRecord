18def4a7ac3ac91bd2c9265400cafcb87ee5911f
wabc1994 <liuxiongcheng145@gmail.com>
2018-10-04 21:50:24 +0800

    回文数字

--- src/String/PalindromeNumber.java
+++ src/String/PalindromeNumber.java
@@ -3,9 +3,12 @@ package String;
 public class PalindromeNumber {
     public boolean isPalindrome(int x) {
         //小于0的情况是一种情况, 判断左右是否相等的情况下如果进行下去
-        if(x<0)
+        if(x<0) {
             return false;
-        if(x!=0  && x%10==0) return false;
+        }
+        if(x!=0  && x%10==0) {
+            return false;
+        }
         int tmp=0;
         while(x>tmp){
             tmp = tmp *10 +x %10;
