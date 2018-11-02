95a5f8864b7821240f0efb97f9769bdd718ddafd
wabc1994 <liuxiongcheng145@gmail.com>
2018-11-02 21:44:40 +0800

    tok问题进行修改

--- src/TopKFrequentElements.java
+++ src/TopKFrequentElements.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class TopKFrequentElements {
-    public List<Integer> solution(int [] nums, int k){
+    public List<Integer> solution_(int [] nums, int k){
         Map<Integer,Integer> map = new HashMap<>();
         //统计频率的写法,
         for(int n:nums){
@@ -14,7 +14,10 @@ public class TopKFrequentElements {
             //逐渐构建堆的过程，如果堆中的元素比较少于k个，直接添加
 
             if (minheap.size() < k)
+            {
                 minheap.add(entry);
+            }
+
             else {
                 //堆已满，此时如果要新加入的元素value大于堆中的最小值，则添加
                 Map.Entry<Integer, Integer> min = minheap.peek();
@@ -33,5 +36,35 @@ public class TopKFrequentElements {
 
         return res;
         }
+
+        //桶排序算法，空间复杂度降低为O(）
+    public List<Integer> solution_2(int []nums, int k) {
+        List<Integer>[] bucket = new List[nums.length + 1];
+        Map<Integer, Integer> frequencyMap = new HashMap<Integer, Integer>();
+
+        //正确写法情况
+
+        for (int n : nums) {
+            frequencyMap.put(n, frequencyMap.getOrDefault(n, 0) + 1);
+        }
+
+        for (int key : frequencyMap.keySet()) {
+            int frequency = frequencyMap.get(key);
+            if (bucket[frequency] == null) {
+                bucket[frequency] = new ArrayList<>();
+            }
+            bucket[frequency].add(key);
+        }
+
+        List<Integer> res = new ArrayList<>();
+
+        for (int pos = bucket.length - 1; pos >= 0 && res.size() < k; pos--) {
+            if (bucket[pos] != null) {
+                res.addAll(bucket[pos]);
+            }
+        }
+        return res;
+    }
+
 
 }
