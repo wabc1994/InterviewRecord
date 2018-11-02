a896b9899e5f9a7a694fa50eedebbb0b0f82611e
wabc1994 <liuxiongcheng145@gmail.com>
2018-11-02 21:57:55 +0800

    最大堆解决top k 频率问题

---
+++ src/TopKFrequentWords.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class TopKFrequentWords {
+    public List<String> topKFrequent(String[] words, int k) {
+        HashMap<String, Integer > map = new HashMap<>();
+        for (String s : words)  {
+            map.put(s, map.getOrDefault(s,0) + 1);
+        }
+
+        PriorityQueue<Map.Entry<String,Integer>> maxHeap = new PriorityQueue<>(k, (a, b) ->
+                a.getValue()==b.getValue() ? a.getKey().compareTo(b.getKey()) : b.getValue()-a.getValue());
+        // if same frequency, then sort alphabetical .
+
+        for (Map.Entry<String,Integer> entry : map.entrySet() ) {maxHeap.add(entry);}
+
+        List<String> res = new ArrayList<>();
+        while (res.size() < k) {res.add(maxHeap.poll().getKey());}
+        return res;
+    }
+}
